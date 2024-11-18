127c81a71481a5bc11ebb48f4a6b9f80e6c259fc
Pratham Sharma <pratham@engagecreative.ca>
2024-11-18 20:12:22 -0330

    added jotai state management for admin

--- src/components/providers/supabase-provider.tsx
+++ src/components/providers/supabase-provider.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Admin } from "@/db/schema/admins";
+import { adminAtom, isLoadingAtom } from "@/store/admin";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useAtom } from "jotai";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
 
@@ -13,18 +16,61 @@ export const SupabaseProvider = ({
 }) => {
   const [supabase] = useState(() => createClientComponentClient());
   const router = useRouter();
+  const [, setAdmin] = useAtom(adminAtom);
+  const [, setIsLoading] = useAtom(isLoadingAtom);
+
+  const getAdmin = async (email: string) => {
+    try {
+      const response = await fetch("/api/admin/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      });
+
+      if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.message || "Failed to login");
+      }
+
+      const adminData = await response.json();
+      return adminData as Admin;
+    } catch (apiError) {
+      console.error("❌ API Error:", apiError);
+      throw apiError;
+    }
+  };
 
   useEffect(() => {
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange(() => {
+    } = supabase.auth.onAuthStateChange(async (event, session) => {
       router.refresh();
+
+      setIsLoading(true);
+      try {
+        if (session?.user?.email) {
+          const adminData = await getAdmin(session.user.email);
+          console.log("✅ Admin Data:", adminData);
+          setAdmin(adminData);
+        } else {
+          setAdmin(null);
+        }
+      } catch (error) {
+        console.error("Failed to fetch admin data:", error);
+        setAdmin(null);
+      } finally {
+        setIsLoading(false);
+      }
     });
 
     return () => {
       subscription.unsubscribe();
     };
-  }, [router, supabase]);
+  }, [router, supabase, setAdmin, setIsLoading]);
 
   return (
     <SupabaseContext.Provider value={{ supabase }}>
