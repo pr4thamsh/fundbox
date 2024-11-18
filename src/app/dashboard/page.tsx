3961ef576ea09ea879e87a896c78867bc8fa4e24
Pratham Sharma <pratham@engagecreative.ca>
2024-11-17 21:06:16 -0330

    minor changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import { useAuth } from "@/hooks/use-auth";
-
 export default function DashboardPage() {
-  const { user } = useAuth();
-
-  return (
-    <div>
-      <h1>Welcome, {user?.user_metadata.first_name}!</h1>
-      <p>Your email: {user?.email}</p>
-    </div>
-  );
+  return <div></div>;
 }
